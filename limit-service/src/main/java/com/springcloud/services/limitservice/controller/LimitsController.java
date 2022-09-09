17d321036cd9f6a27ad26cf24a925fd0ed389207
Kamal Debnath <dbkamal@gmail.com>
2022-09-09 01:30:27 -0400

    Initial commit.

---
+++ limit-service/src/main/java/com/springcloud/services/limitservice/controller/LimitsController.java
@@ -0,0 +1,23 @@
+package com.springcloud.services.limitservice.controller;
+
+import com.springcloud.services.limitservice.config.Configuration;
+import com.springcloud.services.limitservice.entity.Limits;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    private Configuration configuration;
+
+    @Autowired
+    public LimitsController(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+    }
+}
---
+++ limit-service/src/test/java/com/springcloud/services/limitservice/LimitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.services.limitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
